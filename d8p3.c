206fa8067f9792590b8136990e1d7677ed72cd98
sridevi153 <82940606+sridevi153@users.noreply.github.com>
2021-04-29 16:37:45 +0530

    d8p3.c

---
+++ d8p3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+struct book
+{
+    char title[10];
+    char author[10];
+    float price;
+    int no_pages;
+};
+void read(struct book *sptr) 
+{
+     printf("Enter the title, author name, price and number of pages in order");
+     scanf("%s%s%f%d",sptr->title,sptr->author,&sptr->price,&sptr->no_pages);
+}
+void print(struct book *sptr) 
+{
+    printf("The book details are\n");
+    printf("\n%s\t%s\t%f\t%d",sptr->title,sptr->author,sptr->price,sptr->no_pages); 
+}
+int main()
+{
+   struct book bk;
+   struct book *sptr=&bk;
+   read(sptr);
+   print(sptr);
+   return 0;     
+}
