af8ebf2a6f4d44f0d025c312285eeca1ab654058
sridevi153 <82940606+sridevi153@users.noreply.github.com>
2021-10-21 17:44:23 +0530

    Create joc1p2.c

---
+++ joc1p2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+  int arr[10],size,count,freq[20];
+  int i,j,n=10;
+  printf("enter the array elements\n");
+  for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    freq[i]=-1;
+  }
+  for(i=0;i<n;i++){
+    for(j=i+1;j<n;j++){
+      if(arr[i]==arr[j]){
+        count++;
+        freq[i]=0;
+      }
+    }
+    if(freq[i]!=0){
+    freq[i]=count;
+  }
+  }
+printf("the freauency of each elements in the array is\n");
+  for(i=0;i<n;i++){
+    if(freq[i]!=0){
+      printf("%d's:%d\n",arr[i],freq[i]);
+    }
+  }
+}
